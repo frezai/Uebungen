095bf948e72ec5939466e7d95c709ecd0cb71ee8
Fatma Rezai <frezai@hm.edu>
2017-11-22 21:51:29 +0100

    wieder eine gemeine Aufgabe

---
+++ src/PlusMinusTree.java
@@ -0,0 +1,24 @@
+
+public class PlusMinusTree {
+	
+	public static void main(String[] args) {
+
+		int height = 5;
+		int x = height -1;
+		
+		for (int i = 0; i < height; i++) {
+			for (int j = 0; j < height - i - 1; j++) {
+				System.out.print(".");
+			}
+			for (int k = 0; k <= i + i; k++) {
+				if(i%x == 0 || k%x ==0) {
+					System.out.print("+");
+				} else{
+					System.out.print("-");
+				}	
+			}
+				System.out.println();
+		}
+	}
+
+}
