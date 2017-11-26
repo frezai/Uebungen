acc3d1d8605def6033ba9d41cb133290957e6595
Fatma Rezai <frezai@hm.edu>
2017-11-26 15:30:42 +0100

    hilfe

--- src/PlusMinusTree.java
+++ src/PlusMinusTree.java
@@ -11,7 +11,7 @@ public class PlusMinusTree {
 				System.out.print(".");
 			}
 			for (int k = 0; k <= i + i; k++) {
-				if(i%x == 0 || k%x ==0) { //Inhalt mit Minus und Rahmen mit Plus
+				if(i%x == 0 || k%x ==0 || i%  ==1) { //Inhalt mit Minus und Rahmen mit Plus
 					System.out.print("+");
 				} else{
 					System.out.print("-");
